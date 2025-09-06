e6a0fa9696ca3d1ec16c3baf696ebe55b1bf9fab
MarcoUCU <MARCO.CARLOMAGNO@correo.ucu.edu.uy>
2025-09-06 14:45:35 -0300

    Update GameLogic.cs

--- src/Program/GameLogic.cs
+++ src/Program/GameLogic.cs
@@ -16,7 +16,7 @@ public static class GameLogic
                 int liveNeighbors = CountLiveNeighbors(currentGrid, x, y, width, height);
                 bool isAlive = currentGrid[y, x];
 
-                // Aplicar reglas de Conway
+                // Reglas de Conway
                 if (isAlive && liveNeighbors < 2)
                     newGrid[y, x] = false;
                 else if (isAlive && liveNeighbors > 3)
@@ -34,19 +34,21 @@ public static class GameLogic
     private static int CountLiveNeighbors(bool[,] grid, int x, int y, int width, int height)
     {
         int count = 0;
+        
         for (int i = -1; i <= 1; i++)
         {
             for (int j = -1; j <= 1; j++)
             {
                 if (i == 0 && j == 0) continue;
-
+                
                 int col = (x + i + width) % width;
                 int row = (y + j + height) % height;
-
+                
                 if (grid[row, col])
                     count++;
             }
         }
+        
         return count;
     }
 }
